4f1ab476731a1c9772985d0d4b7cf64808d41ae9
Ronak Pal Singh Bali <ronaqpalsinghbali@gmail.com>
2023-10-15 16:06:46 +0530

    Added subnet id to ec2

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "EC2_instance" {
+
+  ami           = var.ami_windows
+  instance_type = var.instance_type_t2_micro
+  subnet_id = var.subnet_id_ec2
+
+  tags = var.tag_parent
+  
+  volume_tags = var.tag_parent
+}
+
+
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "tag_parent" {
+    type = map(any)
+}
+
+
+variable "ami_windows" {
+  type    = string
+}
+
+
+variable "instance_type_t2_micro" {
+  type    = string
+}
+
+variable "subnet_id_ec2" {
+  type = string
+}
